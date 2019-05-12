f07abf4f376284c2d7318a380d0ea38a2e75014e
daniel <you@example.com>
2019-05-12 12:03:56 -0700

    value-selection

--- sample.py
+++ sample.py
@@ -1,3 +1,4 @@
+import numpy as delete_list_item
 import math
 import threading
 import collections
@@ -20,7 +21,7 @@ class Graph:
   
         # Mark the current node as visited and print it 
         visited = True
-        print v, 
+        print(v) 
   
         # Recur for all the vertices adjacent to 
         # this vertex 
@@ -48,7 +49,7 @@ class Graph:
         return self.graph
 
 class Puzzle:
-    number_of_cubes = 6
+    number_of_cubes = 40
     number_of_faces = 6
 
     set_of_values = []
@@ -57,6 +58,7 @@ class Puzzle:
     array_of_cubes = []
     cube = []
     pair = []
+    solution_array = []
 
     class Node:
         opposite_pair_1 = []
@@ -70,16 +72,58 @@ puzzle = Puzzle()
 def get_value_index(selection_value):
     for i in range (len(puzzle.set_of_values)):
         if(puzzle.set_of_values[i] == selection_value):
-            #print(i)
+            #print(selection_value, i)
             return i 
             
 def mark_selection(index):
+    print('Updated selection count array:', puzzle.selection_count_array)
+    print(' ')
     for i in range (len(puzzle.selection_count_array)):
         if(i == index):
-            if(puzzle.selection_count_array < 2):
-                puzzle.selection_count_array += 1
-            else: 
-                print("Error: too many selections of a value made")
+            if(puzzle.selection_count_array[i] < 2):
+                puzzle.selection_count_array[i] += 1
+        '''
+            else:
+                #print(' ') 
+                #print("Error: too many selections of value", puzzle.set_of_values[i])
+                #print(' ') 
+        '''
+
+def make_selection(array_of_cubes):
+    for i in range (len(array_of_cubes)):
+        for j in range (len(array_of_cubes[i])):
+            correct_solution_found = check_selection_correctness(array_of_cubes[i][j])
+            if (correct_solution_found):
+                puzzle.solution_array.append(array_of_cubes[i][j])
+                for k in range (len(array_of_cubes[i][j])):
+                    mark_selection(get_value_index(array_of_cubes[i][j][k]))                    
+                break
+
+def remove_half_solution(half_solution):
+    for i in range (len(half_solution)):
+        for j in range (len(puzzle.array_of_cubes[i])):
+            if (half_solution[i] == puzzle.array_of_cubes[i][j]):
+                print(puzzle.array_of_cubes[i][j])
+                
+    '''    
+        for j in range (len(puzzle.array_of_cubes[i])):
+            for k in range (len(puzzle.array_of_cubes[j])):
+                if (half_solution[i] == puzzle.array_of_cubes[j][k]):
+                    #print(half_solution[i], puzzle.array_of_cubes[i][j])
+                    #print(i, puzzle.array_of_cubes[i][j])
+                    print(puzzle.array_of_cubes[j][k])
+                    #delete_list_item.delete(puzzle.array_of_cubes, i, j)
+                    #del(puzzle.array_of_cubes[i][j])
+    '''
+
+def check_selection_correctness(cube_pair_selection):
+    selection_is_correct = False
+    for i in range (len(cube_pair_selection)):
+        value_index = get_value_index(cube_pair_selection[i])
+        for i in range(value_index):
+            if (i == (value_index - 1) and puzzle.selection_count_array[i] < 2):
+                selection_is_correct = True
+    return selection_is_correct
 
 def get_value_set():
     set_of_values = []
@@ -111,18 +155,20 @@ def get_selection_counter():
     return selection_count_array
 
 def assign_coloration():
-
     for i in range ((puzzle.number_of_cubes * puzzle.number_of_faces)):
 
         puzzle.value_list.append(get_puzzle_one_coloration(i))
 
-        #puzzle.pair.append(get_puzzle_one_coloration(i))
-        puzzle.cube.append(get_puzzle_one_coloration(i))
+        puzzle.pair.append(get_puzzle_one_coloration(i))
+
+        if ((i + 1) % 2 == 0):
+            puzzle.cube.append(puzzle.pair)
+            puzzle.pair = []
 
         if((i + 1) % 6 == 0):
             puzzle.array_of_cubes.append(puzzle.cube)
             puzzle.cube = []
-                      
+                               
 def get_puzzle_one_coloration(cube_face):
     return(1 + (math.floor(cube_face * math.pi) % 30))    
 
@@ -136,31 +182,68 @@ def get_puzzle_four_coloration(cube_face):
     return(1 + (math.floor(cube_face * math.sqrt(5)) % 30))        
 
 def main():
-    #thread1 = threading.Thread(target=traverse_graph, name='T1')
-    #thread2 = threading.Thread(target=traverse_graph, name='T2')
-
-    #thread1.start()
-    #thread2.start()
-
-    #thread1.join()
-    #thread2.join()
-
     assign_coloration()
+
+    print(' ')
+    print('Array of cubes')
+    print('--------------')
     print(puzzle.array_of_cubes)
     print(' ')
 
+    print('Set of values')
+    print('--------------')
     puzzle.set_of_values = get_value_set()
     print(puzzle.set_of_values)
     print(' ')
 
+    print('Count of each value')
+    print('-------------------')
     print(get_value_count(get_value_set()))
     print(' ')
 
-    #puzzle.selection_counter_array = get_selection_counter()
-    #print(puzzle.selection_counter_array)
-    #print(' ')
-    #mark_selection(get_value_index(29.0))
-    
+    puzzle.selection_count_array = get_selection_counter()
+
+    print("Number of values: ", len(puzzle.set_of_values), ',', "number of cubes: ", len(puzzle.array_of_cubes))
+    print(' ')
+
+    if(len(puzzle.set_of_values) < len(puzzle.array_of_cubes)):
+        print("Values are unequal to cubes,", "minimum obstacle at least", len(puzzle.set_of_values))
+        print(' ')
+
+        while(len(puzzle.set_of_values) < len(puzzle.array_of_cubes)):
+            puzzle.array_of_cubes.pop()
+
+        print('Reduced array of cubes')
+        print('----------------------')
+        print(puzzle.array_of_cubes)
+        print(' ')
+
+    print('Starting selection process')
+    print('--------------------------')
+    make_selection(puzzle.array_of_cubes)
+
+    print('Half solution 1')
+    print('---------------')
+    print(puzzle.solution_array)
+    print(' ')
+
+    remove_half_solution(puzzle.solution_array)
+
+    print('New array of cubes with half solution removed')
+    print('---------------------------------------------')
+    print(puzzle.array_of_cubes)
+    print(' ')
+
+    print('Starting selection process')
+    print('--------------------------')
+    make_selection(puzzle.array_of_cubes)
+
+    print('Half solution 2')
+    print('---------------')
+    print(puzzle.solution_array)
+    print(' ')
+
+'''
     g = Graph() 
 
     for i in range (len((puzzle.array_of_cubes))):
@@ -175,13 +258,12 @@ def main():
                 #print(int(source),int(destination))
                 g.addEdge(int(source),int(destination))
                 
-    print "Following is Depth First Traversal"
+    print ("Following is Depth First Traversal")
     g.DFS()
     print(' ')
     print(g.return_graph()) 
     print(' ')
-
-    print("Number of values:", len(puzzle.set_of_values), "Number of cubes:", len(puzzle.array_of_cubes))
+'''
 
 if __name__ == "__main__": 
     main()
